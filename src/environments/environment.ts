6d5389d5674662b12e2521a399045daf0c70914a
kwamkert <tomriddle477@gmail.com>
2020-04-09 14:12:42 +0000

    environment variables checked

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const environment = {
 
 
   production: false,
-  api_host: "https://church-spring.herokuapp.com/api/v1",  
+  //api_host: "https://church-spring.herokuapp.com/api/v1",  
+  api_host: "http://localhost:8081/api/v1",  
   image_client_id: "33010a9ec2dfe99",
   image_cient_secret: "7ecb0482009105b232eaf139efc06564810b036e",
 
