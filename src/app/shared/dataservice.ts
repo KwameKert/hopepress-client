e57d2c94048ebe0752826647609b14f509d2b58b
kwamkert <tomriddle477@gmail.com>
2020-04-19 16:57:43 +0000

    updating dataservice

--- src/app/shared/dataservice.ts
+++ src/app/shared/dataservice.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class DataService {
+
+
+ 
+private _baseUrl :String = environment.api_host;    
 
 private messageSource = new BehaviorSubject('Dashboard');
 
 private countDown = new BehaviorSubject('000000')
 
-constructor(){}
+constructor(private _httpClient: HttpClient){}
 
 
 
@@ -17,8 +23,6 @@ constructor(){}
 getCountDown(): Observable<any>{
     return this.countDown.asObservable();
 }
-
-
 setCountDown(time: string){
     this.countDown.next(time);
 }
@@ -29,9 +33,11 @@ getLink(): Observable<any> {
 
 
 setLink(linkName: string){
-
     this.messageSource.next(linkName)
-
 }
+
+public getNextEvent(): Observable<any>{
+    return this._httpClient.get(`${this._baseUrl}/dashboard/nextEvent`);
+  }
 
 } 
